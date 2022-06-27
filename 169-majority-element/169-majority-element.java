34840e448d171e9650c6ff7584dff7978f96f500
mahesh-sketch <81766631+mahesh-sketch@users.noreply.github.com>
2022-06-27 23:12:20 +0530

    Time: 2080 ms (5.01%), Space: 57.4 MB (6.90%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,23 +1,37 @@
 class Solution {
     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         int count=1;
+//         int a=0;
+//         Arrays.sort(nums);
+//         if(n==1){
+//             return nums[0];
+//         }
+//         for(int i=0;i<n-1;i++){
+//             if(nums[i]==nums[i+1]){
+//            count++;
+//                 if(count>n/2){
+//                     a=nums[i];
+//                 }
+//             }
+//             else{
+//                 count=1;
+//             }
+            
+//         }
+//         return a;
         int n=nums.length;
+         int a=0;
+        for(int i=0;i<n;i++){
         int count=1;
-        int a=0;
-        Arrays.sort(nums);
-        if(n==1){
-            return nums[0];
-        }
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-           count++;
-                if(count>n/2){
-                    a=nums[i];
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
                 }
             }
-            else{
-                count=1;
+            if(count>n/2){
+                a = nums[i];
             }
-            
         }
         return a;
     }
