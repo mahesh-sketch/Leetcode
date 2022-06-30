2ec42fabdf9e3fc0b19a22bc7a3e4d77d0979740
mahesh-sketch <81766631+mahesh-sketch@users.noreply.github.com>
2022-06-30 20:36:51 +0530

    Time: 45 ms (38.24%), Space: 93.8 MB (47.37%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -10,7 +10,7 @@ class Solution {
             }
             else{
                 int j=h.get(nums[i]); //if contains then return the index of that duplicate value
-                if(Math.abs(i-j)<=k){ // check that abs of that is less than k if then return true
+                if(Math.abs(i-j)<=k && nums[i]==nums[j]){ // check that abs of that is less than k if then return true
                     return true;
                 }
                 h.put(nums[i],i);
